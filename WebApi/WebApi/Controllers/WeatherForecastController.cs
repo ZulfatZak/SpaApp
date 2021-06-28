d08c5fe3d8f6c4e71270f9a8ba95baddfb31a6ed
Zulfat Zakirov <31z@mail.ru>
2021-06-28 12:03:07 +0300

    First commit

---
+++ WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly AppDatabaseContext _appDatabaseContext;
+
+        public WeatherForecastController(AppDatabaseContext appDatabaseContext)
+        {
+            _appDatabaseContext = appDatabaseContext;
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    var rng = new Random();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            User user = new User { Name = value, Age = 22 };
+            _appDatabaseContext.Users.Add(user);
+            _appDatabaseContext.SaveChanges();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _appDatabaseContext.Users.Find(id).Name;
+        }
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var users = _appDatabaseContext.Users.Select(u => u.Name).ToArray();
+            return users;
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            User user = _appDatabaseContext.Users.Find(id);
+            user.Name = value;
+            _appDatabaseContext.SaveChanges();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            User user = _appDatabaseContext.Users.Find(id);
+            _appDatabaseContext.Users.Remove(user);
+            _appDatabaseContext.SaveChanges();
+        }
+    }
+}
---
+++ WebApi/WebApi/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDbContext<AppDatabaseContext>(o => o.UseSqlServer($"Data Source=localhost;Initial Catalog={nameof(AppDatabaseContext)};Integrated Security=True"));
+            services.AddDbContext<AppDatabaseContext>(o => o.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SpaApp;Trusted_Connection=True;MultipleActiveResultSets=true"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
